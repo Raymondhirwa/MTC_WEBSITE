2d2ae6d39e6886b9daa28771a96bef79c536d873
basonis55i <basonis55i@gmail.com>
2026-05-13 12:39:47 +0200

    connect frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json()); // To parse JSON bodies
-app.use(cors()); // To allow frontend requests
+app.use(
+    cors({
+        origin: '*'
+    })
+);
 
 // Email Transporter Configuration
 const transporter = nodemailer.createTransport({
